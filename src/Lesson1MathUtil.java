cd474f7ee5f6f738a34963195fb1d5a18373cea4
MerikeAK <merikeaspe@hotmail.com>
2020-08-25 18:19:51 +0300

    25.08.2020 uus

--- src/Lesson1MathUtil.java
+++ src/Lesson1MathUtil.java
@@ -1,16 +1,67 @@
+import java.util.Scanner;
 public class Lesson1MathUtil {
 
-    public static void main(String[] args) {
-        System.out.println(min(5, 7));
-        System.out.println(max(5,7));
-        System.out.println(abs(-30));
-        System.out.println(isEven(3));
-        System.out.println(min(2, 5, 7));
-        System.out.println(max (44, 34, 7));
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which method to use");
+        String test = scanner.nextLine();
+
+        if(test.equalsIgnoreCase("min")) {
+            System.out.println("Please enter first number");
+            int a = scanner.nextInt();
+            System.out.println("Please enter second number");
+            int b = scanner.nextInt();
+            System.out.println("Min value: " + min(a, b));
+        }
+        else if(test.equalsIgnoreCase("max")) {
+            System.out.println("Please enter first number");
+            int a = scanner.nextInt();
+            System.out.println("Please enter second number");
+            int b = scanner.nextInt();
+            System.out.println("Max value: " + max(a, b));
+        }
+        else if(test.equalsIgnoreCase("abs")) {
+            System.out.println("Please enter number");
+            int a = scanner.nextInt();
+            System.out.println("Absolute value: " + abs(a));
+        }
+        else if(test.equalsIgnoreCase("isEven")) {
+            System.out.println("Please enter number");
+            int a = scanner.nextInt();
+            System.out.println("Even value: " + isEven(a));
+        }
+        else if(test.equalsIgnoreCase("minOfThree")) {
+            System.out.println("Please enter first number");
+            int a = scanner.nextInt();
+            System.out.println("Please enter second number");
+            int b = scanner.nextInt();
+            System.out.println("Please enter third number");
+            int c = scanner.nextInt();
+            System.out.println("Min value: " + minOfThree(a, b, c));
+        }
+        else if(test.equalsIgnoreCase("maxOfThree")) {
+            System.out.println("Please enter first number");
+            int a = scanner.nextInt();
+            System.out.println("Please enter second number");
+            int b = scanner.nextInt();
+            System.out.println("Please enter third number");
+            int c = scanner.nextInt();
+            System.out.println("Max value: " + maxOfThree(a, b, c));
+        }
+
+
+
+        //System.out.println(min(5, 7));
+        //System.out.println(max(5,7));
+        //System.out.println(abs(-30));
+        //System.out.println(isEven(3));
+        //System.out.println(min(2, 5, 7));
+        //System.out.println(max (44, 34, 7));
     }
 
     public static int min(int a, int b){
         // TODO tagasta a ja b väikseim väärtus
+
         if (a>b){
             return b;
         }
@@ -52,7 +103,7 @@ public class Lesson1MathUtil {
         return false;
     }
 
-    public static int min(int a, int b, int c) {
+    public static int minOfThree(int a, int b, int c) {
         // TODO tagasta a, b ja c väikseim väärtus
         if (a<=b && a<=c){
             return a;
@@ -64,7 +115,7 @@ public class Lesson1MathUtil {
 
     }
 
-    public static int max(int a, int b, int c){
+    public static int maxOfThree(int a, int b, int c){
         // TODO tagasta a, b ja c suurim väärtus
         if (a>=b && a>=c){
             return a;
--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,19 +1,34 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Lesson2 {
 
     public static void main(String[] args) {
-        exercise1();
-        exercise2(4);
-        exercise3(2, 5);
-        fibonacci(5);
-        exercise5();
+        //exercise1();
+        //exercise2(5);
+        //exercise3(3, 3);
+        //System.out.println(fibonacci(5));
+        //exercise5();
+        exercise7();
     }
 
     public static void exercise1() {
         // TODO loo 10 elemendile täisarvude massiv
         // TODO loe sisse konsoolist 10 täisarvu
         // TODO trüki arvud välja vastupidises järiekorras
+        int[] a = new int[10];
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter 10 numbers");
+        for (int i = 0; i < 10; i++) {
+            int number = scanner.nextInt();
+            a[i] = number;
+        }
+        for (int b = a.length - 1; b > 0; b--) {
+            System.out.println(a[b]);
+        }
     }
 
     public static void exercise2(int x) {
@@ -21,6 +36,14 @@ public class Lesson2 {
         // Näide:
         // Sisend 5
         // Väljund 2 4 6 8 10
+        int abc = 0;
+        for (int i = 0; i < 60; i += 2) {
+            if (abc < x) {
+                System.out.println(i + " ");
+                abc += 1;
+            }
+        }
+
     }
 
     public static void exercise3(int x, int y) {
@@ -30,6 +53,14 @@ public class Lesson2 {
         // 1 2 3
         // 2 4 6
         // 3 6 9
+        int[][] multipTable = new int[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.print((i + 1) * (j + 1));
+            }
+            System.out.println(" ");
+        }
+
     }
 
     public static int fibonacci(int n) {
@@ -37,31 +68,57 @@ public class Lesson2 {
         // Fibonacci jada on fib(n) = fib(n-1) + fib(n-2);
         // 0, 1, 1, 2, 3, 5, 8, 13, 21
         // Tagasta fibonacci jada n element
-        return 0;
-    }
+        int[] fibonacci = new int[n];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        for (int i = 2; i < n; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+        for (int m = 0; m < fibonacci.length; m++) {
+            System.out.print(fibonacci[m]);
+        }
+        System.out.println(" ");
+        return fibonacci[n - 1];
+        }
 
-    public static void exercise5() {
-        // https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=36
-    }
+        public static void exercise5 () {
+            // https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=36
+            //1. input n 2. print n 3. if n = 1 then STOP 4. if n is odd then n ←− 3n + 1 5. else n ←− n/2 6. GOTO 2
 
-    public static void exercise6() {
+        }
+
+        public static void exercise6 () {
         /*
             Kirjutada Java programm, mis loeb failist visits.txt sisse looduspargi külastajad erinevatel jaanuari päevadel ning
             a) sorteerib külastuspäevad külastajate arvu järgi kasvavalt ning prindib tulemuse konsoolile;
             b) prindib konsoolile päeva, mil külastajaid oli kõige rohkem.
             Faili asukoht tuleb programmile ette anda käsurea parameetrina.
          */
-    }
+        }
 
-    public static void exercise7() {
-        // TODO arvuta kasutades BigDecimali 1.89 * ((394486820340 / 15 ) - 4 )
-        BigDecimal a = new BigDecimal(1.89);
-        BigDecimal b = new BigDecimal("394486820345");
-        BigDecimal c = new BigDecimal("15");
-        BigDecimal d = new BigDecimal("4");
-    }
+        public static void exercise7 () {
+            // TODO arvuta kasutades BigDecimali 1.89 * ((394486820340 / 15 ) - 4 )
+            BigDecimal a = new BigDecimal("1.89");
+            BigDecimal b = new BigDecimal("394486820340");
+            BigDecimal c = new BigDecimal("15");
+            BigDecimal d = new BigDecimal("4");
 
-    public static void exercise8() {
+            //res1 =b/c
+            //res2=res1 - d
+            //res3=a * res2
+
+            BigDecimal res1 = b.divide(c, RoundingMode.DOWN);
+
+            BigDecimal res2 = res1.subtract(d);
+
+            BigDecimal res3 = a.multiply(res2);
+            System.out.println(res3);
+
+
+
+        }
+
+        public static void exercise8 () {
         /*
         Failis nums.txt on üksteise all 150 60-kohalist numbrit.
 
@@ -71,12 +128,12 @@ public class Lesson2 {
         VASTUS:
         Õige summa: 77378062799264987173249634924670947389130820063105651135266574
          */
-    }
+        }
 
-    public static void exercise9() {
+        public static void exercise9 () {
         /* TODO
         Sama mis eelmises ülesandes aga ära kasuta BigInt ega BigDecimal klassi
          */
-    }
+        }
 
-}
+    }
